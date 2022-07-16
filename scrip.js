efafe23813416ce1447b7d5966568ffa4023ff4b
fredmesquitta <106532654+fredmesquitta@users.noreply.github.com>
2022-07-15 22:02:59 -0300

    nova

---
+++ scrip.js
@@ -0,0 +1,9 @@
+let menubar = document .querySelector('#menu-bar')
+let navbar = document .querySelector('.navbar')
+
+
+
+menubar.onclick = () =>{
+    menubar.classList.toggle('fa-times')
+    nav.classList.toggle('active')
+}
